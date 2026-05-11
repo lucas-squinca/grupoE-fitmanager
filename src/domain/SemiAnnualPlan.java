653e321baf3942b09b7ce8269e5187903401c3e2
lucas-squinca <lucassquinca35@gmail.com>
2026-05-11 20:22:04 -0300

    Adicionar subclasse SemiAnnualPlan ao pacote Domain.

---
+++ src/domain/SemiAnnualPlan.java
@@ -0,0 +1,18 @@
+package domain;
+
+public class SemiAnnualPlan extends Plan {
+    public SemiAnnualPlan(String name, String description, int minDurationMonths, double pricePerMonth) {
+        super(name, description, minDurationMonths, pricePerMonth);
+    }
+
+    @Override
+    public double calculateTotalPrice(int months) {
+        double total = getPricePerMonth() * months;
+        return total * 0.90;
+    }
+
+    @Override
+    public double getCancellationFee(Enrollment enrollment) {
+        return 0.0;
+    }
+}
