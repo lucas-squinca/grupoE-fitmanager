df3ef338489b577945cde2ba2704dbb0763b4981
lucas-squinca <lucassquinca35@gmail.com>
2026-05-11 20:12:35 -0300

    Modificar conteúdo de Plan.java (domain) para tornÃa-la uma clase abstrata

--- src/domain/Plan.java
+++ src/domain/Plan.java
@@ -1,61 +1,29 @@
 package domain;
 
-public class Plan {
+public abstract class Plan {
     private String name;
     private String description;
-    private PlanType type;
     private int minDurationMonths;
     private double pricePerMonth;
 
-    public Plan(String name, String description, PlanType type, int minDurationMonths, double pricePerMonth) {
+    public Plan(String name, String description, int minDurationMonths, double pricePerMonth) {
         this.name = name;
         this.description = description;
-        this.type = type;
         this.minDurationMonths = minDurationMonths;
         this.pricePerMonth = pricePerMonth;
     }
 
-    public double calculateTotalPrice(int months) {
-        return this.pricePerMonth * months;
-    }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public int getMinDurationMonths() { return minDurationMonths; }
+    public double getPricePerMonth() { return pricePerMonth; }
 
     public void updatePrice(double newPrice) {
-        this.pricePerMonth = newPrice;
+        if (newPrice > 0) {
+            this.pricePerMonth = newPrice;
+        }
     }
 
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public PlanType getType() {
-        return this.type;
-    }
-
-    public void setType(PlanType type) {
-        this.type = type;
-    }
-
-    public int getMinDurationMonths() {
-        return this.minDurationMonths;
-    }
-
-    public void setMinDurationMonths(int minDurationMonths) {
-        this.minDurationMonths = minDurationMonths;
-    }
-
-    public double getPricePerMonth() {
-        return this.pricePerMonth;
-    }
+    public abstract double calculateTotalPrice(int months);
+    public abstract double getCancellationFee(Enrollment enrollment);
 }
